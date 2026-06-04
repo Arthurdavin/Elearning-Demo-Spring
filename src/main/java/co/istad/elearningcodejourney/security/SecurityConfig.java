cf5a614a8dfcc9af2b6fd466ca69ddbe71881d05
Arthurdavin <davinchhayml@gmail.com>
2026-06-04 23:32:11 +0700

    add security

--- src/main/java/co/istad/elearningcodejourney/security/SecurityConfig.java
+++ src/main/java/co/istad/elearningcodejourney/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package co.istad.elearningcodejourney.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -10,27 +11,33 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain apiSecurity(HttpSecurity http) {
-        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
-        http.authorizeHttpRequests(endpoints -> endpoints
+    @Bean
+    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+
+        http.oauth2ResourceServer(oauth2 ->
+                oauth2.jwt(Customizer.withDefaults()));
+
+        http.authorizeHttpRequests(auth -> auth
                 .requestMatchers(
                         "/swagger-ui/**",
                         "/swagger-ui.html",
                         "/v3/api-docs/**"
-                )
+                ).permitAll()
+
+                .requestMatchers(HttpMethod.GET, "/api/v1/categories/**")
                 .permitAll()
-                .requestMatchers("/api/v1/categories/**")
-                .permitAll()
+
                 .anyRequest()
                 .authenticated()
         );
 
-        http.sessionManagement(state -> state
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(AbstractHttpConfigurer::disable);
+
         return http.build();
     }
 }
